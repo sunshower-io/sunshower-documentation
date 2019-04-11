40cfb681467768b533723d5179f62c3c9f6818d2
Josiah Haswell <josiah.haswell@gmail.com>
2019-04-11 17:32:39 -0600

    Initial import

---
+++ .vuepress/config.js
@@ -0,0 +1,39 @@
+const glob = require('glob'),
+    fs = require('fs');
+
+// markdownFiles = glob.sync('docs/**/*.md').map(f => '/' + rename(f));
+
+function rename(n) {
+    if (n.indexOf("README") != -1) {
+        return '';
+    }
+    let pos = n.lastIndexOf('.');
+    return n.substring(0, pos) + '.html';
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.substr(1);
+}
+
+function tree(root) {
+    let subdirs = fs.readdirSync(root),
+        subgroups = subdirs.map(subdir => {
+            return {
+                title: capitalize(subdir),
+                collapsable: true,
+                children: glob.sync(`${root}/${subdir}/**/*.md`).map(f => '/' + rename(f))
+            }
+        });
+    return subgroups;
+}
+
+module.exports = {
+    title: 'Sunshower.io',
+    description: 'Docs for Sunshower.io',
+    themeConfig: {
+        logo: '/assets/images/logo.svg',
+        sidebar: tree('docs/en')
+    },
+};
+
+
---
+++ README.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: /assets/images/logo.svg
+actionText: Get Started →
+actionLink: /docs/en/
+description: Beautifully Simple Cloud Management
+features:
+- title: Cost Optimization 
+  details: Manage Your Cloud Costs in Seconds
+- title: Cloud Management 
+  details: Gain Deep Insights into your Cloud Expenditures
+- title: Self-Serve 
+  details: Look, Ma!  No expensive consultants!
+footer: MIT Licensed | Copyright © 2018-present Sunshower.io 
+---
