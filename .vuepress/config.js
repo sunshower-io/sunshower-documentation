2c7d1a3ff69f9354cbe967d97bd9172a99cbe259
Josiah Haswell <josiah.haswell@gmail.com>
2019-04-11 19:59:27 -0600

    Adding support for ordering pages

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,6 @@
 const glob = require('glob'),
+    fm = require('front-matter'),
     fs = require('fs');
-
-// markdownFiles = glob.sync('docs/**/*.md').map(f => '/' + rename(f));
 
 function rename(n) {
     if (n.indexOf("README") != -1) {
@@ -15,13 +14,36 @@ function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.substr(1);
 }
 
+function sort(files) {
+    let orders = [];
+    for(let file of files) {
+
+
+        let content = fs.readFileSync(file, "utf8"),
+            frontMatter = fm(content),
+            attrs = frontMatter && frontMatter.attributes,
+            order = attrs && attrs.order;
+        if(order) {
+            orders.push({order: order, name: file});
+        } else {
+            orders.push({order: 0, name: file});
+            console.warn(`Warning: File ${file} does not have an order.  Defaulting to 0`);
+        }
+    }
+    let a = orders.sort((lhs, rhs) => {
+        return lhs.order - rhs.order
+    }).map(t => `/${rename(t.name)}`);
+    return a;
+
+}
+
 function tree(root) {
     let subdirs = fs.readdirSync(root),
         subgroups = subdirs.map(subdir => {
             return {
                 title: capitalize(subdir),
                 collapsable: true,
-                children: glob.sync(`${root}/${subdir}/**/*.md`).map(f => '/' + rename(f))
+                children: sort(glob.sync(`${root}/${subdir}/**/*.md`))
             }
         });
     return subgroups;
--- pages/en/guide/introduction.md
+++ pages/en/guide/introduction.md
@@ -1,2 +1,4 @@
-
+---
+order: 1
+---
 # Introduction
